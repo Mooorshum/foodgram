a8c4ed40956e9e0bb14240f7c695ebd8b682a5f4
Mooorshum <stas.basov@whatever.com>
2024-08-31 23:08:05 +0300

    formated ingredients json file

---
+++ backend/data/format_json_data.py
@@ -0,0 +1,21 @@
+import json
+
+with open('ingredients.json', 'r', encoding='utf-8') as file:
+    data = json.load(file)
+
+transformed_data = [
+    {
+        "model": "recipes.ingredient",
+        "pk": index + 1,
+        "fields": {
+            "name": ingredient["name"],
+            "measurement_unit": ingredient["measurement_unit"]
+        }
+    }
+    for index, ingredient in enumerate(data)
+]
+
+with open('transformed_ingredients.json', 'w', encoding='utf-8') as file:
+    json.dump(transformed_data, file, ensure_ascii=False, indent=4)
+
+print("Transformation complete! Check 'transformed_ingredients.json' for the output.")
