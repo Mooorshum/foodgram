66b372de0cfb64061489dec98d43e511e90f1dd0
Mooorshum <stas.basov@whatever.com>
2024-09-05 21:07:14 +0300

    trying to make short link button work x9991999191

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,7 +2,6 @@ from django.contrib.auth import update_session_auth_hash
 from django.db.models import Sum
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect
-from django.urls import reverse
 from django_filters import rest_framework as filters
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
@@ -10,6 +9,8 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+import pyclip
 
 from api.filters import RecipeFilter
 from api.mixins import AddRemoveMixin
@@ -211,27 +212,11 @@ class RecipeViewSet(viewsets.ModelViewSet, AddRemoveMixin):
             recipe_link,
             context={'request': request}
         )
-        short_link = serializer.data['short-link']
-        response_data = {
-            'short-link': short_link,
-            'message': 'Short link copied to clipboard!',
-            'script': f"""
-                <script>
-                    function copyToClipboard(text) {{
-                        const tempInput = document.createElement('input');
-                        tempInput.value = text;
-                        document.body.appendChild(tempInput);
-                        tempInput.select();
-                        document.execCommand('copy');
-                        document.body.removeChild(tempInput);
-                        alert('Short link copied to clipboard: ' + text);
-                    }}
-                    copyToClipboard('{short_link}');
-                </script>
-            """
-        }
-        return Response(response_data, status=status.HTTP_200_OK)
-    
+        short_link = serializer.data.get('short-link', '')
+        if short_link:
+            pyclip.copy(short_link)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
     @action(
         detail=True, methods=['post', 'delete'],
         url_path='favorite', permission_classes=[IsAuthenticated]
@@ -286,8 +271,8 @@ class RecipeRedirectView(APIView):
 
     def get(self, request, link, *args, **kwargs):
         recipe_link = get_object_or_404(RecipeLink, link=link)
-        recipe_detail_url = reverse('recipes-detail', args=[recipe_link.recipe.id])
-        return redirect(recipe_detail_url)
+        recipe = recipe_link.recipe
+        return redirect('recipe-detail', pk=recipe.pk)
 
 
 class FavouriteViewSet(viewsets.ModelViewSet):
