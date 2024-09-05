69de86c1b82f86fc336f1e6e4efce8d99e1fd8cd
Mooorshum <stas.basov@whatever.com>
2024-09-05 23:59:41 +0300

    OOOOh

--- backend/api/views.py
+++ backend/api/views.py
@@ -268,7 +268,7 @@ class RecipeRedirectView(APIView):
     def get(self, request, link, *args, **kwargs):
         recipe_link = get_object_or_404(RecipeLink, link=link)
         recipe = recipe_link.recipe
-        recipe_detail_url = reverse('recipes-detail', kwargs={'pk': recipe.pk})
+        recipe_detail_url = reverse('recipes-detail', kwargs={'pk': recipe.id})
         return redirect(recipe_detail_url)
 
 
