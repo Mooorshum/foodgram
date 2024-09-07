7cb489010dd5fe6b157d96a97158cef58d61539a
Mooorshum <stas.basov@whatever.com>
2024-09-07 22:24:58 +0300

    changed order of patterns in main urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,11 +4,11 @@ from django.urls import include, path
 from api.views import RecipeRedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
     path(
         's/<str:link>/',
         RecipeRedirectView.as_view(),
         name='recipe-redirect'
     ),
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
 ]
